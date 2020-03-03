1f5a21c74c098890991694c567a005da90309e4e
Matias Pankow <matipankow@gmail.com>
2020-03-03 00:56:19 -0300

    + AntD for style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
+import { Layout, Row, Col, Modal, Button, Form, Input } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 import UserTable from './tables/UserTable'
 import AddUserForm from './forms/AddUserForm'
 import EditUserForm from './forms/EditUserForm'
 
 const App = () => {
+
+  const { Header, Content, Footer } = Layout;
 
   const usersData = [
     { id: 1, name: 'Tania', username: 'floppydiskette' },
@@ -14,8 +18,10 @@ const App = () => {
 
   // getters and setters
   const [users, setUsers] = useState(usersData)
+  const [visible, setVisible] = useState(false)
   const [editing, setEditing] = useState(false)
   const [currentUser, setCurrentUser] = useState(initialFormState)
+  const [form] = Form.useForm();
 
   const addUser = user => {
     user.id = users.length + 1
@@ -29,9 +35,17 @@ const App = () => {
   }
 
   const editRow = user => {
-    setEditing(true)
-  
     setCurrentUser({ id: user.id, name: user.name, username: user.username })
+    form.setFieldsValue({name: user.name, username: user.username})
+    setEditing(true)
+    setVisible(true)
+  }
+
+  const newRow = () => {
+    setCurrentUser(initialFormState)
+    form.setFieldsValue({name: initialFormState.name, username: initialFormState.username})
+    setEditing(false)
+    setVisible(true)
   }
 
   const updateUser = (id, updatedUser) => {
@@ -40,34 +54,109 @@ const App = () => {
     setUsers(users.map(user => (user.id === id ? updatedUser : user)))
   }
 
+  const handleOk = e => {
+    console.log(e);
+    if (editing) {
+      updateUser(currentUser.id, currentUser)
+    } else {
+      addUser(currentUser)
+    }
+
+    setVisible(false)
+    form.resetFields()
+  };
+
+  const handleCancel = e => {
+    console.log(e);
+    setVisible(false)
+    form.resetFields()
+  };
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+
+    setCurrentUser({ ...currentUser, [name]: value })
+    console.log(currentUser);
+    
+  }
+
+
   return (
-    <div className="container">
-      <h1>CRUD App with Hooks</h1>
-      <div className="flex-row">
-      <div className="flex-large">
-        {editing ? (
-          <div>
-            <h2>Edit user</h2>
-            <EditUserForm
-              editing={editing}
-              setEditing={setEditing}
-              currentUser={currentUser}
-              updateUser={updateUser}
-            />
-          </div>
-        ) : (
-          <div>
-            <h2>Add user</h2>
-            <AddUserForm addUser={addUser} />
-          </div>
-        )}
-      </div>
-        <div className="flex-large">
-          <h2>View users</h2>
-          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
-        </div>
-      </div>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <h1 className="header">Simple CRUD with Hooks</h1>
+      </Header>
+      <Content className="container">
+          <Modal
+            title={editing ? "Edit user" : "Add user"}
+            visible={visible}
+            onOk={() => {
+              form
+                .validateFields()
+                .then(values => {
+                  form.resetFields();
+                  handleOk();
+                })
+                .catch(info => {
+                  console.log('Validate Failed:', info);
+                });
+            }}
+            onCancel={handleCancel}
+          >
+
+          <Form
+            form={form}
+            layout="vertical"
+            name="userForm"
+          >
+            <Form.Item
+              name="name"
+              label="Name"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the name of user!',
+                },
+              ]}
+            >
+              <Input name="name" onChange={handleInputChange} />
+            </Form.Item>
+            <Form.Item 
+              name="username" 
+              label="Username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the username of user!',
+                },
+              ]}
+            >
+              <Input name="username" onChange={handleInputChange} />
+            </Form.Item>
+          </Form>
+          </Modal>
+          <Row>
+            <Col span={24}>
+              <Row>
+              <Col span={21}>
+                <h2>View users</h2>
+              </Col>
+              <Col span={3}>
+                <Button className="btn-add-user" onClick={newRow} type="primary" shape="round" icon={<PlusOutlined />}>
+                  Add user
+                </Button>
+              </Col>
+              </Row>
+              <Row>
+                <Col span={24}>
+                  <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+      </Content>
+      <Footer className="footer">Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
   )
 }
 
